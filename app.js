e99824e6db9d2e526ea9d8981af938993aa52aa1
hamidrezamt <hamidreza6773@gmail.com>
2017-11-27 15:55:20 +0330

    nothing important

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("hello world");
+var R = require("r-script");
+var dataFrame = require("dataframe-js");
+var out = R("test.R").data(4).callSync();
+console.log(dataFrame(out))
+    //console.log(JSON.parse(JSOout[0]));
---
+++ test.r
@@ -0,0 +1,5 @@
+testvar <- input[[1]] + 2
+# arde <- 8
+oo <- c(1,2,3)
+ss <- c("a","b","c")
+df <- data.frame(oo,ss)
