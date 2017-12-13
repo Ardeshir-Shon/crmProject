a29340195317c471a6a5be20b4119e7925cce8b8
user <ad.ardeshir@gmail.com>
2017-12-13 18:33:00 +0330

    error handled.

--- app.js
+++ app.js
@@ -37,7 +37,12 @@ app.listen(3000, function() {
 var R = require("r-script");
 var out = R("RModules/1_extractRFM.R").data(__dirname.replace(/\\/g, '/')).callSync();
 var out = R("RModules/2_normalization.R").data(__dirname.replace(/\\/g, '/')).callSync();
-var out = R("RModules/3_optimumNumber.R").data(__dirname.replace(/\\/g, '/')).callSync();
+try {
+    var out = R("RModules/3_optimumNumber.R").data(__dirname.replace(/\\/g, '/')).callSync();
+}
+catch (err){
+    console.log("plots created ...")
+}
 
 console.log(__dirname.replace(/\\/g, '/'))
     //console.log(JSON.parse(JSOout[0]));
