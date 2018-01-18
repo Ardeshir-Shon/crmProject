641b783c2ec525051e8b1ef92b35b3251c94d0f7
hamidrezamt <hamidreza6773@gmail.com>
2018-01-18 15:30:40 +0330

    commit for nothing!

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var formidable = require('formidable');
 var fs = require('fs')
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-var arraylist= require("arraylist");
+var arraylist = require("arraylist");
 var R = require("r-script");
-let ExplinCluster= require("./ExplainCluster.js")
+let ExplinCluster = require("./ExplainCluster.js")
 let config = require('./public/javascripts/config.js');
 var con = mysql.createConnection(config);
 app.set('view engine', 'handlebars');
@@ -22,7 +22,7 @@ app.engine('handlebars', expressHandlebars({
     }
 }));
 
-var clusterAnalysis=new arraylist;
+var clusterAnalysis = new arraylist;
 
 
 // var tempExplain=new ExplinCluster(155,"این دسته از کاربران کاربران وفادار و پر سود ده هستند که از همه نظر بهترین و ارزشمند ترین کابران ما هستند و باید آنها را راضی نگه داشت.");
@@ -68,34 +68,34 @@ app.post('/upload', function(req, res) {
     form.on('file', function(field, file) {
         fs.rename(file.path, path.join(form.uploadDir, "trans-df.csv"));
         console.log("uploaded.")
-            var out = R("RModules/1_extractRFM.R").data(__dirname.replace(/\\/g, '/')).callSync();
-            var out = R("RModules/2_normalization.R").data(__dirname.replace(/\\/g, '/')).callSync();
-            console.log(out);
-            minMaxValues=out;
-            console.log(minMaxValues.split(";")[3])// 3 is max of F as you can understand
-            try {
-                var out = R("RModules/3_optimumNumber.R").data(__dirname.replace(/\\/g, '/')).callSync();
-            } catch (err) {
-                console.log("plots created ...")
-            }
-            var out = R("RModules/4_clusterEvaluation.R").data(__dirname.replace(/\\/g, '/')).callSync();
-            console.log("clusters evaluated ...")
-            var evaluateClusters=out;
-            console.log(out);
-            var k=evaluateClusters.split(";")[0];
-            console.log(evaluateClusters);
-            console.log(k);
-            console.log(evaluateClusters.split(";")[1]);
-            for (var i=1;i<=k;i++){
-                //console.log(evaluateClusters.split(";")[i])
-                var sql = "SELECT `desc` FROM class_desc WHERE type = '" + evaluateClusters.split(";")[i] + "';"
-                con.query(sql, function(err, result) {
-                    if (err) throw err; // type not defined in DB
-                    clusterAnalysis.add(JSON.parse(JSON.stringify(result[0].desc)));
-                });
-            }
-            console.log(__dirname.replace(/\\/g, '/'))
-                //console.log(JSON.parse(JSOout[0]));
+        var out = R("RModules/1_extractRFM.R").data(__dirname.replace(/\\/g, '/')).callSync();
+        var out = R("RModules/2_normalization.R").data(__dirname.replace(/\\/g, '/')).callSync();
+        console.log(out);
+        minMaxValues = out;
+        console.log(minMaxValues.split(";")[3]) // 3 is max of F as you can understand
+        try {
+            var out = R("RModules/3_optimumNumber.R").data(__dirname.replace(/\\/g, '/')).callSync();
+        } catch (err) {
+            console.log("plots created ...")
+        }
+        var out = R("RModules/4_clusterEvaluation.R").data(__dirname.replace(/\\/g, '/')).callSync();
+        console.log("clusters evaluated ...")
+        var evaluateClusters = out;
+        console.log(out);
+        var k = evaluateClusters.split(";")[0];
+        console.log(evaluateClusters);
+        console.log(k);
+        console.log(evaluateClusters.split(";")[1]);
+        for (var i = 1; i <= k; i++) {
+            //console.log(evaluateClusters.split(";")[i])
+            var sql = "SELECT `desc` FROM class_desc WHERE type = '" + evaluateClusters.split(";")[i] + "';"
+            con.query(sql, function(err, result) {
+                if (err) throw err; // type not defined in DB
+                clusterAnalysis.add(JSON.parse(JSON.stringify(result[0].desc)));
+            });
+        }
+        console.log(__dirname.replace(/\\/g, '/'))
+            //console.log(JSON.parse(JSOout[0]));
         res.end('success');
     });
 
@@ -129,10 +129,3 @@ app.post('/RFMRange', function(req, res) {
 app.listen(3000, function() {
     console.log("Working on port 3000");
 });
-
-// var max = 12;
-// var min = 120;
-
-
-// module.exports.min = min;
-// module.exports.max = max;
